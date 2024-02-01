7ab12dc538e44975cd84cd3170daafefb94b8317
Chaeyeon Seo <ssallyseo@gmail.com>
2024-02-01 17:48:29 +0900

    JdbcTemplate

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -28,10 +29,11 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){ //인터페이스
 //        return new MemoryMemberRepository(); //구현체
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource); // 순수 jdbc
         /*
         DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다. 스프링 부트는 데이터베이스 커넥션 정보를
         바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다
         */
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
