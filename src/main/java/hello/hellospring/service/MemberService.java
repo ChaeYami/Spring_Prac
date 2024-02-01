294c65ff3d6462c32fd4cd6ff128852cee7ecf6a
Chaeyeon Seo <ssallyseo@gmail.com>
2024-02-01 18:58:36 +0900

    AOP 미적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,6 +8,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+
+
 @Transactional // JPA를 통한 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다.
 public class MemberService {
 
@@ -18,12 +20,30 @@ public class MemberService {
     }
 
     // 1. 회원가입
+
     public Long join(Member member){
+
+        /*
         // 같은 이름 x
         validateDuplicateMember(member);
 
         memberRepository.save(member);
         return member.getId();
+        */
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -38,11 +58,25 @@ public class MemberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다");
                 });
     }
+
     // 2. 전체 회원 조회
+
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+
+//        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+
     }
+
     // 3. 특정 회원 조회
+
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
